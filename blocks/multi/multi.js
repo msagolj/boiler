7f8c4085477fdabd1edfbc1cd32b3aa6a23adedc
msagolj <msagolj@adobe.com>
2025-09-02 10:14:15 +0200

    update test blokc

--- blocks/multi/multi.js
+++ blocks/multi/multi.js
@@ -1,3 +1,15 @@
 export default function decorate(block) {
-  
+  const div = document.createElement('div');
+  div.classList.add('block-options');
+  const key = document.createElement('div');
+  key.textContent = 'Block Options';
+  const value = document.createElement('div');
+
+  let classes = Array.from(block.classList)
+    .filter(cls => cls !== 'block' && cls !== 'multi');
+  value.textContent = classes.join(' ');
+
+  div.append(key, value);
+  // append div as first child of block
+  block.insertBefore(div, block.firstChild);
 }
