d11f10e916da15ff2805ef073554b2cb5c65af34
msagolj <msagolj@adobe.com>
2025-09-02 11:56:12 +0200

    render field names

--- blocks/multi/multi.js
+++ blocks/multi/multi.js
@@ -1,4 +1,24 @@
-export default function decorate(block) {
+export default async function decorate(block) {
+  
+
+  // load the component-models.json file
+  const componentModels = await fetch(`${window.hlx.codeBasePath}/component-models.json`);
+  if (componentModels.ok) {
+      const data = await componentModels.json();
+      // find the multi model
+      const multiModel = data.find(model => model.id === 'multi');
+      // get the list of filed names , except for the ones where the component is set to tab
+      const fieldNames = multiModel.fields.filter(field => field.component !== 'tab' && !field.name.startsWith('classes')).map(field => field.name);
+      // to each row in the block add a div with then name of the field
+      block.querySelectorAll(':scope > div').forEach((row, index) => {
+          const div = document.createElement('div');
+          div.textContent = fieldNames[index];
+          // add as first child of row
+          row.insertBefore(div, row.firstChild);
+      });
+    }
+
+    // create block header with block options
   const div = document.createElement('div');
   div.classList.add('block-options');
   const key = document.createElement('div');
