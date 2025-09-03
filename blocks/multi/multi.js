819ebf336dfbd953f1ad1c62d04e2291f79e71da
msagolj <msagolj@adobe.com>
2025-09-03 12:28:26 +0200

    support grouping when rendering properties

--- blocks/multi/multi.js
+++ blocks/multi/multi.js
@@ -2,7 +2,7 @@ export default async function decorate(block) {
   
 
   // load the component-models.json file
- /* const componentModels = await fetch(`${window.hlx.codeBasePath}/component-models.json`);
+  const componentModels = await fetch(`${window.hlx.codeBasePath}/component-models.json`);
   if (componentModels.ok) {
       const data = await componentModels.json();
       // find the multi model
@@ -10,15 +10,36 @@ export default async function decorate(block) {
       // get the list of filed names , except for the ones where the component is set to tab
       const fieldNames = multiModel.fields.filter(field => field.component !== 'tab' && !field.name.startsWith('classes')).map(field => field.name);
       // to each row in the block add a div with then name of the field
-      block.querySelectorAll(':scope > div').forEach((row, index) => {
+      let previousGroupName = '';
+      let previousDiv = null;
+      let rowIndex = 0;
+      fieldNames.forEach((fieldName, index) => {
+        console.log(fieldName);
+        // if it has a grouping prefix
+        if (fieldName.includes('_')) {
+          const groupName = fieldName.split('_')[0];
+          if (groupName === previousGroupName) {
+            previousDiv.textContent = `${previousDiv.textContent} ${fieldName}`;
+          } else {
+            const div = document.createElement('div');
+            div.textContent = fieldName;
+            block.children[rowIndex].insertBefore(div, block.children[rowIndex].firstChild);
+            previousGroupName = groupName;
+            previousDiv = div;
+            rowIndex++;
+          }
+        } else {
+          previousGroupName = '';
+          previousDiv = null;
           const div = document.createElement('div');
-          div.textContent = fieldNames[index];
+          div.textContent = fieldName;
           // add as first child of row
-          row.insertBefore(div, row.firstChild);
+          block.children[rowIndex].insertBefore(div, block.children[rowIndex].firstChild);
+          rowIndex++;
+        }
       });
     }
-  */
- 
+
     // create block header with block options
   const div = document.createElement('div');
   div.classList.add('block-options');
